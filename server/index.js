c9c7d4f76b90c1e78316dfcc177c331f92dd4bfb
Brandon Parker <brandon.ch.parker@gmail.com>
2021-07-01 15:23:40 -0700

    Implemented working queries for reviews, and meta data.

--- server/index.js
+++ server/index.js
@@ -20,16 +20,16 @@ app.listen(port, (err) => {
 app.use(compression());
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-    // res.send('test');
-    pool.query('SELECT * FROM reviews LIMIT 5', (err, queryRes) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(queryRes.rows);
-      }
-    //   pool.end();
-    });
+app.get('/test/:id', (req, res) => {
+    res.send(req.params.id);
+    // pool.query('SELECT * FROM reviews LIMIT 5', (err, queryRes) => {
+    //   if (err) {
+    //     res.send(err);
+    //   } else {
+    //     res.send(queryRes.rows);
+    //   }
+    // //   pool.end();
+    // });
 })
 
 //UNION ALL SELECT url FROM photos WHERE `
@@ -44,61 +44,199 @@ app.get('/test', (req, res) => {
     // });
 // })
 
-app.get('/reviews/:id', (req, res) => {
-    const instructions = `
-      SELECT
-        reviews.id,
-        product_id,
-        rating,
-        date,
-        summary,
-        body,
-        recommend,
-        reported,
-        reviewer_name,
-        reviewer_email,
-        response,
-        helpfulness,
-        url
-      FROM
-        reviews
-      LEFT JOIN reviews_photos
+app.get('/reviews', (req, res) => {
+  const {product_id, page = 1, count = 5, sort = 'date'} = req.query;
+  // sort by newest = date, helpful = helpfulness, or relevant = date/helpful
+  // implement relevant
+  const instructions = `
+    SELECT
+      reviews.id,
+      product_id,
+      rating,
+      date,
+      summary,
+      body,
+      recommend,
+      reported,
+      reviewer_name,
+      reviewer_email,
+      response,
+      helpfulness,
+      json_agg(json_build_object ('id', reviews_photos.id, 'url', reviews_photos.url)) AS photos
+    FROM
+      reviews
+    LEFT JOIN reviews_photos
       ON reviews_photos.review_id = reviews.id
-      WHERE
-        product_id = '${req.params.id}'
-    `;
+    WHERE
+      product_id = '${product_id}'
+    GROUP BY reviews.id
+    ORDER BY 
+      ${sort}
+    LIMIT ${page * count}
+  `;
 
-    // const instructions = `
-    //   SELECT * FROM reviews WHERE product_id = '${req.params.id}'
-    //   UNION ALL
-    //   SELECT url FROM reviews_photos WHERE reviews_photos.review_id = reviews.id
-    // `;
+  // const instructions = `
+  //   SELECT * FROM reviews WHERE product_id = '${req.params.id}'
+  //   UNION ALL
+  //   SELECT url FROM reviews_photos WHERE reviews_photos.review_id = reviews.id
+  // `;
 
-    pool.query(instructions, (err, queryRes) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(queryRes.rows);
-        }
-      });
+  pool.query(instructions, (err, queryRes) => {
+    if (err) {
+      console.log(err)
+        res.send(err);
+    } else {
+        res.send(queryRes.rows);
+    }
+  });
+});
+
+app.get('/reviews/meta', (req, res) => {
+  const {product_id} = req.query;
+  const instructions1 = `
+    SELECT
+      reviews.product_id,
+      json_build_object (
+        '1', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 1),
+        '2', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 2),
+        '3', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 3),
+        '4', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 4),
+        '5', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 5)
+      ) AS ratings,
+      json_build_object (
+        true, COUNT (reviews.recommend) FILTER (where reviews.recommend = true),
+        false, COUNT (reviews.recommend) FILTER (where reviews.recommend = false)
+      ) AS recommended
+    FROM
+      reviews
+    LEFT JOIN characteristic_reviews
+      ON characteristic_reviews.review_id = reviews.id
+    WHERE
+      reviews.product_id = '${product_id}'
+    GROUP BY reviews.product_id
+  `;
+
+  const instructions2 = `
+    SELECT
+      characteristics.char_name,
+      json_build_object (
+        'id', AVG(characteristics.id),
+        'value', AVG(rating)
+      ) AS statistics
+        
+    FROM
+      characteristics
+    LEFT JOIN characteristic_reviews
+      ON characteristic_reviews.characteristic_id = characteristics.id
+    WHERE
+      product_id = '${product_id}'
+    GROUP BY characteristics.char_name
+  `
+
+  pool.query(instructions1)
+    .then((queryRes) => {
+      pool.query(instructions2)
+      .then((queryRes2) => {
+        let characteristicObject = {};
+        queryRes2.rows.forEach((obj) => {
+          characteristicObject[obj.char_name] = obj.statistics
+        })
+        queryRes.rows[0].characteristics = characteristicObject;
+        res.send(queryRes.rows[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.send(err)
+      })
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send(err)
+    })
+});
 
 
-    // pool.query(instructions, (err, queryRes) => {
-    //   if (err) {
-    //       res.send(err);
-    //   } else {
-    //     //   res.send(queryRes.rows[1].id);
-    //     pool.query(`SELECT url FROM reviews_photos WHERE review_id = ${queryRes.rows[1].id}`, (err, query2Res) => {
-    //         if (err) {
-    //             res.send(err);
-    //         } else {
-    //             res.send(query2Res);
-    //         }
-    //     })
-    //   }
-    // });
-})
 
+  // pool.query(instructions1, (err, queryRes) => {
+  //   if (err) {
+  //     console.log(err);
+  //     res.send(err);
+  //   } else {
+  //     pool.query(instructions2, (err, queryRes2) => {
+  //       if (err) {
+  //         console.log(err);
+  //         res.send(err);
+  //       } else {
+  //         let characteristicObject = {};
+  //         queryRes2.rows.forEach((obj) => {
+  //           characteristicObject[obj.char_name] = obj.statistics
+  //         })
+  //         queryRes.rows[0].characteristics = characteristicObject;
+  //         res.send(queryRes.rows[0]);
+  //       }
+  //     });
+  //   }
+  // });
+
+//   const instructions1 = `
+//   SELECT
+//     reviews.product_id,
+//     json_build_object (
+//       '1', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 1),
+//       '2', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 2),
+//       '3', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 3),
+//       '4', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 4),
+//       '5', COUNT (reviews.rating) FILTER (WHERE reviews.rating = 5)
+//     ) AS ratings,
+//     json_build_object (
+//       true, COUNT (reviews.recommend) FILTER (where reviews.recommend = true),
+//       false, COUNT (reviews.recommend) FILTER (where reviews.recommend = false)
+//     ) AS recommended,
+//     json_object_agg(
+//       char_name, 'obj'
+//     ) AS characteristics
+//   FROM
+//     reviews
+//   LEFT JOIN characteristic_reviews
+//     ON characteristic_reviews.review_id = reviews.id
+//   LEFT JOIN characteristics
+//     ON characteristics.id = characteristic_reviews.characteristic_id
+//   WHERE
+//     reviews.product_id = '${product_id}'
+//   GROUP BY reviews.product_id
+// `;
+
+  // pool.query(instructions2, (err, queryRes) => {
+  //   if (err) {
+  //     console.log(err);
+  //       res.send(err);
+  //   } else {
+  //       res.send(queryRes.rows);
+  //   }
+  // });
+//   const instructions = `
+//     SELECT
+//       json_object_agg(
+//         char_name, 'obj'
+//       ) AS characteristics
+//     FROM
+//       characteristics
+//     WHERE
+//       characteristics.product_id = '${product_id}'
+//   `;
+
+//   const characteristicName = `
+//   SELECT
+//     characteristics.char_name
+//   FROM
+//     characteristics
+//   WHERE
+//     product_id = '${product_id}'
+// `
+
+
+
+///// ///// /////
 // const client = new Client({
 //   user: 'dbuser',
 //   host: 'database.server.com',
